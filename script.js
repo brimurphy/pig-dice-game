5c893fe2945552c67573325ad16f44c2642e283d
Brian <blpmurphy@gmail.com>
2021-05-14 22:19:29 +0100

    Add functionality to switch active player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const scoreEl0 = document.querySelector('#score--0');
 const scoreEl1 = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
@@ -15,7 +17,9 @@ scoreEl0.textContent = 0;
 scoreEl1.textContent = 0;
 diceEl.classList.add(`hidden`);
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -28,9 +32,16 @@ btnRoll.addEventListener('click', function () {
   // Check if no.1 rolled
   if (dice !== 1) {
     currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
     // Update later
-    current0El.textContent = currentScore;
+    // current0El.textContent = currentScore;
   } else {
     // if true switch player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
