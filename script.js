2c5aafa467bf789d51917d1d5d6626d570700952
Brian <blpmurphy@gmail.com>
2021-05-17 21:18:27 +0100

    Add restart game function and refactor code

--- script.js
+++ script.js
@@ -12,6 +12,29 @@ const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
+let scores, currentScore, activePlayer, playing;
+
+// Starting condition
+function init() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  scoreEl0.textContent = 0;
+  scoreEl1.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add(`hidden`);
+
+  player0El.classList.remove(`player--winner`);
+  player1El.classList.remove(`player--winner`);
+  player0El.classList.add(`player--active`);
+  player1El.classList.remove(`player--active`);
+}
+
+init();
+
 function switchPlayer() {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -19,16 +42,6 @@ function switchPlayer() {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 }
-
-// Starting condition
-scoreEl0.textContent = 0;
-scoreEl1.textContent = 0;
-diceEl.classList.add(`hidden`);
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -52,6 +65,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
+// Holding score
 btnHold.addEventListener('click', function () {
   if (playing) {
     // Add current score to players total score
@@ -61,7 +75,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // Check if players score is >=100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 10) {
       // Finish game
       playing = false;
       diceEl.classList.add(`hidden`);
@@ -73,6 +87,8 @@ btnHold.addEventListener('click', function () {
         .classList.remove(`player--active`);
     }
     // Switch player
-    switchPlayer();
-  }
+  } else switchPlayer();
 });
+
+// Restart/New game
+btnNew.addEventListener('click', init);
